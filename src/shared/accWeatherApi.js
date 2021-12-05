cc7a7f9f7c4d98924c4a4fab76d3aadfc4791727
alex-1pro <pro1.alex.b@gmail.com>
2021-12-05 12:56:53 +0200

    changed apiKey

--- src/shared/accWeatherApi.js
+++ src/shared/accWeatherApi.js
@@ -1,6 +1,5 @@
-// export const API_KEY = "Rlivcvu8LqidxlnDZlg5d1dGFL5N4p95"
 //export const API_KEY = "9WNZsVTjUH7OfCV8vCmWir9rnGELFggu"
-export const API_KEY = "Rlivcvu8LqidxlnDZlg5d1dGFL5N4p95"
+export const API_KEY = "MiGTVt9Xdh6vttWqVfrCSx5DoDaRN0Ja"
 export const CURRENT_WEATHER_URI = "https://dataservice.accuweather.com/currentconditions/v1/"
 export const CITY_URI = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?"
 export const DAILY_FORECASTS_URI = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
